6187f3b399520febf757de8d8615355dde8ce627
DHay10 <son-ta@hotmail.fr>
2022-11-21 11:51:43 -0500

    Create faq.php

---
+++ ZAWSS/app/views/Main/faq.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <style>
+        .container {
+        margin-top: 1.5%;
+        }
+    </style>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FAQ</title>
+</head>
+<body>
+<!-- <?php //include 'app\views\includes\header.php'; ?> -->
+
+<div class="container">
+
+        <!--Section: FAQ-->
+    <section>
+    <h3 class="text-center mb-4 pb-2 text-primary fw-bold">FAQ</h3>
+    <p class="text-center mb-5">
+        Find the answers for the most frequently asked questions below
+    </p>
+
+    <div class="row">
+        <div class="col-md-6 col-lg-4 mb-4">
+        <h6 class="mb-3 text-primary"><i class="far fa-paper-plane text-primary pe-2"></i> What are the most popular travel destinations booked on zwass?</h6>
+        <p>
+            <strong><u>We have plenty of popular travel destinations!</u></strong> 
+            the travel agencies we book most often are to to southeastern Asia...
+        </p>
+        </div>
+
+        <div class="col-md-6 col-lg-4 mb-4">
+        <h6 class="mb-3 text-primary"><i class="fas fa-pen-alt text-primary pe-2"></i> question here</h6>
+        <p>
+            <strong><u>good question</u></strong> this is a very good question Lorem Ipsum is simply dummy text of the printing 
+            and typesetting industry. Lorem Ipsum has been the industry's standard dummy text 
+            ever since the 1500s, when an unknown printer took a galley of type and scrambled 
+            it to make a type specimen book.
+        </p>
+        </div>
+
+        <div class="col-md-6 col-lg-4 mb-4">
+        <h6 class="mb-3 text-primary"><i class="fas fa-user text-primary pe-2"></i> A simple
+            question?
+        </h6>
+        <p>
+            this is also a very good question. Lorem Ipsum is simply dummy text of the printing 
+            and typesetting industry. Lorem Ipsum has been the industry's standard dummy text 
+            ever since the 1500s, when an unknown printer took a galley of type and scrambled 
+            it to make a type specimen book.
+        </p>
+        </div>
+
+        <div class="col-md-6 col-lg-4 mb-4">
+        <h6 class="mb-3 text-primary"><i class="fas fa-rocket text-primary pe-2"></i> Something is something
+        </h6>
+        <p>
+            i don't know what to write here. Lorem Ipsum is simply dummy text of the printing 
+            and typesetting industry. Lorem Ipsum has been the industry's standard dummy text 
+            ever since the 1500s, when an unknown printer took a galley of type and scrambled 
+            it to make a type specimen book.
+        </p>
+        </div>
+
+        <div class="col-md-6 col-lg-4 mb-4">
+        <h6 class="mb-3 text-primary"><i class="fas fa-home text-primary pe-2"></i> A simple
+            question?
+        </h6>
+        <p><strong><u>Unfortunately,</u>.</strong> we ran out of questions to fill this example FAQ page . 
+        Lorem Ipsum is simply dummy text of the printing 
+            and typesetting industry. Lorem Ipsum has been the industry's standard dummy text 
+            ever since the 1500s, when an unknown printer took a galley of type and scrambled 
+            it to make a type specimen book.
+        </p>
+        </div>
+
+        <div class="col-md-6 col-lg-4 mb-4">
+        <h6 class="mb-3 text-primary"><i class="fas fa-book-open text-primary pe-2"></i> this last question</h6>
+        <p>
+            very nice question indeed :D ! Lorem Ipsum is simply dummy text of the printing 
+            and typesetting industry. Lorem Ipsum has been the industry's standard dummy text 
+            ever since the 1500s, when an unknown printer took a galley of type and scrambled 
+            it to make a type specimen book.
+        </p>
+        </div>
+    </div>
+    </section>
+    <!--Section: FAQ-->
+</div>
+</body>
+</html>
