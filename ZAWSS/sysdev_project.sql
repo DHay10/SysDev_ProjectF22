d82bea3f6575460d45592b5e0cf2cd31598b5346
Md Saqliyan Islam <82405606+SaqSaq815@users.noreply.github.com>
2022-11-22 23:27:49 -0500

    update sql

--- ZAWSS/sysdev_project.sql
+++ ZAWSS/sysdev_project.sql
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `sysdev_project`
 --
+CREATE DATABASE IF NOT EXISTS `sysdev_project` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `sysdev_project`;
 
 -- --------------------------------------------------------
 
