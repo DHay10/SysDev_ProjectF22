f8c0ba545d7f796a3e9612d68cddb5971c17f1da
DHay10 <son-ta@hotmail.fr>
2022-11-22 22:24:05 -0500

    Updated bootstrap & Created Login Page

--- ZAWSS/app/views/Includes/userHeader.php
+++ ZAWSS/app/views/Includes/userHeader.php
@@ -1,64 +1,44 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <!-- Container wrapper -->
-  <div class="container-fluid">
-    <!-- Toggle button -->
-    <button
-      class="navbar-toggler"
-      type="button"
-      data-mdb-toggle="collapse"
-      data-mdb-target="#navbarRightAlignExample"
-      aria-controls="navbarRightAlignExample"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <i class="fas fa-bars"></i>
-    </button>
-
-    <!-- Collapsible wrapper -->
-    <div class="collapse navbar-collapse" id="navbarRightAlignExample">
-      <!-- Left links -->
-      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <!-- Navbar dropdown -->
-        <li class="nav-item dropdown">
-          <a
-            class="nav-link dropdown-toggle"
-            href="#"
-            id="navbarDropdown"
-            role="button"
-            data-mdb-toggle="dropdown"
-            aria-expanded="false"
-          >
-            Dropdown
-          </a>
-          <!-- Dropdown menu -->
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li>
-              <a class="dropdown-item" href="#">Action</a>
-            </li>
-            <li>
-              <a class="dropdown-item" href="#">Another action</a>
-            </li>
-            <li><hr class="dropdown-divider" /></li>
-            <li>
-              <a class="dropdown-item" href="#">Something else here</a>
-            </li>
-          </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled"
-            >Disabled</a
-          >
-        </li>
-      </ul>
-      <!-- Left links -->
+<nav class="navbar navbar-dark navbar-expand-lg bg-dark">
+    <!-- Container -->
+    <div class="container-fluid">
+        <!-- Brand -->
+        <a class="navbar-brand" href="/Main/index">ZAWSS</a>
+        <!-- Collapse Button -->
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <!-- Nav Items -->
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="/Main/index">Home</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="/User/book">Book</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        More
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="/Main/aboutUs">About Us</a></li>
+                        <li><a class="dropdown-item" href="/Main/faq">FAQ</a></li>
+                        <li><a class="dropdown-item" href="/Main/contactUs">Contact Us</a></li>
+                    </ul>
+                </li>
+                <?php if(isset($_SESSION['user_id'])) { ?>
+                <li class="nav-item">
+                    <a class="nav-link" href="/User/profile">Profile</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/User/logout">Log Out</a>
+                </li>
+                <?php } else { ?>
+                <li class="nav-item">
+                    <a class="nav-link" href="/User/login">Sign In</a>
+                </li>
+                <?php } ?>
+            </ul>
+        </div>
     </div>
-    <!-- Collapsible wrapper -->
-  </div>
-  <!-- Container wrapper -->
 </nav>
---
+++ ZAWSS/app/views/User/login.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Sign In</title>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <!-- CSS only -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+        
+        <!-- JavaScript Bundle with Popper -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    </head>
+
+    <body>
+        <div class="container text-center">
+            <div class="form-floating mb-3">
+                <input type="text" class="form-control" id="username" placeholder="" required>
+                <label for="username">Username</label>
+            </div>
+            <div class="form-floating">
+                <input type="password" class="form-control" id="password" placeholder="" required>
+                <label for="password">Password</label>
+            </div>
+        </div>
+
+        <!-- <div class="vh-100 d-flex justify-content-center align-items-center">
+            <div class="col-md-4 p-5 shadow-sm border rounded-3">
+                <h2 class="text-center mb-4 text-primary">Login Form</h2>
+                <form>
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Email address</label>
+                        <input type="email" class="form-control border border-primary" id="exampleInputEmail1" aria-describedby="emailHelp">
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputPassword1" class="form-label">Password</label>
+                        <input type="password" class="form-control border border-primary" id="exampleInputPassword1">
+                    </div>
+                    <p class="small"><a class="text-primary" href="forget-password.html">Forgot password?</a></p>
+                    <div class="d-grid">
+                        <button class="btn btn-primary" type="submit">Login</button>
+                    </div>
+                </form>
+                <div class="mt-3">
+                    <p class="mb-0  text-center">Don't have an account? <a href="signup.html"
+                            class="text-primary fw-bold">Sign
+                            Up</a></p>
+                </div>
+            </div>
+        </div> -->
+    </body>
+</html>
