baf0255ab9739c2b7b1a9b89ec0f14cfc8fb9cf0
Md Saqliyan Islam <82405606+SaqSaq815@users.noreply.github.com>
2022-11-23 01:23:14 -0500

    worked on views

--- ZAWSS/app/controllers/User.php
+++ ZAWSS/app/controllers/User.php
@@ -1,8 +1,7 @@
 <?php
     namespace app\controllers;
-    class User extends \app\core\Controller {
-<<<<<<< Updated upstream
-        
+    class User extends \app\core\Controller{
+
         public function login() {
             if(isset($_POST['action'])){
                 $user = new \app\models\User();
@@ -14,35 +13,10 @@
                 }else{
                     header('location:/User/login?error=Invalid credentials');
                 }
-=======
-
-
-
-
-        public function logout() {
-
-        }
-
-        public function book() {
-            $this->view('User/booking');
-        }
-
-
-    public function login(){
-    
-        if(isset($_POST['action'])){
-            $user = new \app\models\User();
-            $user = $user->getUser($_POST['username']);
-            if(password_verify($_POST['password'],$user->password_hash)){
-                $_SESSION['user_id'] = $user->user_id;
-                $_SESSION['username'] = $user->username;
-                // header('location:/User/home');
->>>>>>> Stashed changes
             }else{
                 $this->view('User/login');
             }
         }
-<<<<<<< Updated upstream
 
         public function register() {
             if(isset($_POST['action'])){
@@ -61,26 +35,6 @@
                     }else{
                         header('location:/User/register?error=Username is already taken');  
                     }
-=======
-    }
-
-    
-
-    public function register(){
-        if(isset($_POST['action'])){
-            if($_POST['password'] == $_POST['password_conf']){
-                $user = new \app\models\User();
-                $nameUsed = $user->getUser($_POST['username']);
-                if(!$nameUsed){
-                    $user->username = $_POST['username'];
-                    $user->password_hash = password_hash($_POST['password'], PASSWORD_DEFAULT);
-                    $user->fname = $_POST['fname'];
-                    $user->lname = $_POST['lname'];
-                    $user->phone_num = $_POST['phone_num'];
-                    $user->email = $_POST['email'];
-                    $user->insertUser();
-                    header('location:/User/login?message=Registered successfully');
->>>>>>> Stashed changes
                 }else{
                     header('location:/User/register?error=Passwords do not match');
                 }
@@ -90,7 +44,7 @@
         }
         public function logout() {
             session_destroy();
-		    header('location:/User/login');
+            header('location:/User/login');
         }
 
         public function book() {
@@ -100,5 +54,5 @@
                 $this->view('User/booking');
             }
         }
-
     }
+    
