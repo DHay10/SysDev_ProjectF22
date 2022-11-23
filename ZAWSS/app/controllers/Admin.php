ada2463d5b019057142ec247530dd8b0c45f264d
DHay10 <son-ta@hotmail.fr>
2022-11-23 00:09:12 -0500

    Changed Bootstrap Imports

--- ZAWSS/app/controllers/Admin.php
+++ ZAWSS/app/controllers/Admin.php
@@ -3,11 +3,10 @@ namespace app\controllers;
 
 class Admin extends \app\core\Controller{
 	public function login(){
-	
 		if(isset($_POST['action'])){
 			$admin = new \app\models\Admin();
-			$admin = $user->getUser($_POST['username']);
-			if(password_verify($_POST['password'],$admin->password_hash)){
+			$admin = $admin->getAdmin($_POST['username']);
+			if(password_verify($_POST['password'], $admin->password_hash)){
 				$_SESSION['admin_id'] = $admin->admin_id;
 				$_SESSION['username'] = $admin->username;
 				// header('location:/Admin/');
@@ -24,11 +23,11 @@ class Admin extends \app\core\Controller{
 		if(isset($_POST['action'])){
 			if($_POST['password'] == $_POST['password_conf']){
 				$admin = new \app\models\Admin();
-					$admin->username = $_POST['username'];
-					$admin->password_hash = password_hash($_POST['password'], PASSWORD_DEFAULT);
-					$admin->insertAdmin();
-					header('location:/Admin/login?message=Registered successfully');
-			}else{
+				$admin->username = $_POST['username'];
+				$admin->password_hash = password_hash($_POST['password'], PASSWORD_DEFAULT);
+				$admin->insertAdmin();
+				header('location:/Admin/login?message=Registered successfully');
+			} else {
 				header('location:/User/register?error=Passwords do not match');
 			}
 		}else{
--- ZAWSS/app/controllers/User.php
+++ ZAWSS/app/controllers/User.php
@@ -1,69 +1,57 @@
 <?php
     namespace app\controllers;
     class User extends \app\core\Controller {
-        public function login() {
-            $this->view('User/login');
+        
+        public function login(){
+            if(isset($_POST['action'])){
+                $user = new \app\models\User();
+                $user = $user->getUser($_POST['username']);
+                if(password_verify($_POST['password'],$user->password_hash)){
+                    $_SESSION['user_id'] = $user->user_id;
+                    $_SESSION['username'] = $user->username;
+                    // header('location:/User/home');
+                }else{
+                    header('location:/User/login?error=Invalid credentials');
+                }
+            }else{
+                $this->view('User/login');
+            }
         }
 
-        public function register() {
-            
+        public function register(){
+            if(isset($_POST['action'])){
+                if($_POST['password'] == $_POST['password_conf']){
+                    $user = new \app\models\User();
+                    $nameUsed = $user->getUser($_POST['username']);
+                    if(!$nameUsed){
+                        $user->username = $_POST['username'];
+                        $user->password_hash = password_hash($_POST['password'], PASSWORD_DEFAULT);
+                        $user->fname = $_POST['fname'];
+                        $user->lname = $_POST['lname'];
+                        $user->phone_num = $_POST['phone_num'];
+                        $user->email = $_POST['email'];
+                        $user->insertUser();
+                        header('location:/User/login?message=Registered successfully');
+                    }else{
+                        header('location:/User/register?error=Username is already taken');  
+                    }
+                }else{
+                    header('location:/User/register?error=Passwords do not match');
+                }
+            }else{
+                $this->view('User/register');
+            }
         }
-
         public function logout() {
 
         }
 
-        public function book() {
-            $this->view('User/booking');
-        }
-
-        public function login(){
-    
-        if(isset($_POST['action'])){
-            $user = new \app\models\User();
-            $user = $user->getUser($_POST['username']);
-            if(password_verify($_POST['password'],$user->password_hash)){
-                $_SESSION['user_id'] = $user->user_id;
-                $_SESSION['username'] = $user->username;
-                // header('location:/User/home');
-            }else{
-                header('location:/User/login?error=Invalid credentials');
+        public function booking(){
+            if(isset($_POST['action'])){
+                
+            } else {
+                $this->view('User/booking');
             }
-        }else{
-            $this->view('User/login');
         }
 
     }
-
-    public function register(){
-        if(isset($_POST['action'])){
-            if($_POST['password'] == $_POST['password_conf']){
-                $user = new \app\models\User();
-                $nameUsed = $user->getUser($_POST['username']);
-                if(!$nameUsed){
-                    $user->username = $_POST['username'];
-                    $user->password_hash = password_hash($_POST['password'], PASSWORD_DEFAULT);
-                    $user->fname = $_POST['fname'];
-                    $user->lname = $_POST['lname'];
-                    $user->phone_num = $_POST['phone_num'];
-                    $user->email = $_POST['email'];
-                    $user->insertUser();
-                    header('location:/User/login?message=Registered successfully');
-                }else{
-                    header('location:/User/register?error=Username is already taken');  
-                }
-            }else{
-                header('location:/User/register?error=Passwords do not match');
-            }
-        }else{
-            $this->view('User/register');
-        }
-    }
-
-    public function booking(){
-        if(isset($_POST['action'])){
-            
-        }
-    }
-
-}
