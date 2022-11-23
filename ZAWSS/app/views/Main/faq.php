6893cb1c5e630564dad0465333c81b90be56f559
DHay10 <son-ta@hotmail.fr>
2022-11-22 21:12:54 -0500

    test1

--- ZAWSS/app/views/Main/faq.php
+++ ZAWSS/app/views/Main/faq.php
@@ -1,98 +1,101 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <head>
+        <title>FAQ</title>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+        <style>
+            .container {
+            margin-top: 1.5%;
+            }
+        </style>
+        
+        
+    </head>
+    <body>
+        <?php include 'app\views\includes\userHeader.php'; ?>
 
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <style>
-        .container {
-        margin-top: 1.5%;
-        }
-    </style>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>FAQ</title>
-</head>
-<body>
-<!-- <?php //include 'app\views\includes\header.php'; ?> -->
+        <div class="container">
 
-<div class="container">
+                <!--Section: FAQ-->
+            <section>
+            <h3 class="text-center mb-4 pb-2 text-primary fw-bold">FAQ</h3>
+            <p class="text-center mb-5">
+                Find the answers for the most frequently asked questions below
+            </p>
 
-        <!--Section: FAQ-->
-    <section>
-    <h3 class="text-center mb-4 pb-2 text-primary fw-bold">FAQ</h3>
-    <p class="text-center mb-5">
-        Find the answers for the most frequently asked questions below
-    </p>
+            <div class="row">
+                <div class="col-md-6 col-lg-4 mb-4">
+                <h6 class="mb-3 text-primary"><i class="far fa-paper-plane text-primary pe-2"></i> What are the most popular travel destinations booked on zwass?</h6>
+                <p>
+                    <strong><u>We have plenty of popular travel destinations!</u></strong> 
+                    the travel agencies we book most often are to to southeastern Asia...
+                </p>
+                </div>
 
-    <div class="row">
-        <div class="col-md-6 col-lg-4 mb-4">
-        <h6 class="mb-3 text-primary"><i class="far fa-paper-plane text-primary pe-2"></i> What are the most popular travel destinations booked on zwass?</h6>
-        <p>
-            <strong><u>We have plenty of popular travel destinations!</u></strong> 
-            the travel agencies we book most often are to to southeastern Asia...
-        </p>
+                <div class="col-md-6 col-lg-4 mb-4">
+                <h6 class="mb-3 text-primary"><i class="fas fa-pen-alt text-primary pe-2"></i> question here</h6>
+                <p>
+                    <strong><u>good question</u></strong> this is a very good question Lorem Ipsum is simply dummy text of the printing 
+                    and typesetting industry. Lorem Ipsum has been the industry's standard dummy text 
+                    ever since the 1500s, when an unknown printer took a galley of type and scrambled 
+                    it to make a type specimen book.
+                </p>
+                </div>
+
+                <div class="col-md-6 col-lg-4 mb-4">
+                <h6 class="mb-3 text-primary"><i class="fas fa-user text-primary pe-2"></i> A simple
+                    question?
+                </h6>
+                <p>
+                    this is also a very good question. Lorem Ipsum is simply dummy text of the printing 
+                    and typesetting industry. Lorem Ipsum has been the industry's standard dummy text 
+                    ever since the 1500s, when an unknown printer took a galley of type and scrambled 
+                    it to make a type specimen book.
+                </p>
+                </div>
+
+                <div class="col-md-6 col-lg-4 mb-4">
+                <h6 class="mb-3 text-primary"><i class="fas fa-rocket text-primary pe-2"></i> Something is something
+                </h6>
+                <p>
+                    i don't know what to write here. Lorem Ipsum is simply dummy text of the printing 
+                    and typesetting industry. Lorem Ipsum has been the industry's standard dummy text 
+                    ever since the 1500s, when an unknown printer took a galley of type and scrambled 
+                    it to make a type specimen book.
+                </p>
+                </div>
+
+                <div class="col-md-6 col-lg-4 mb-4">
+                <h6 class="mb-3 text-primary"><i class="fas fa-home text-primary pe-2"></i> A simple
+                    question?
+                </h6>
+                <p><strong><u>Unfortunately,</u>.</strong> we ran out of questions to fill this example FAQ page . 
+                Lorem Ipsum is simply dummy text of the printing 
+                    and typesetting industry. Lorem Ipsum has been the industry's standard dummy text 
+                    ever since the 1500s, when an unknown printer took a galley of type and scrambled 
+                    it to make a type specimen book.
+                </p>
+                </div>
+
+                <div class="col-md-6 col-lg-4 mb-4">
+                <h6 class="mb-3 text-primary"><i class="fas fa-book-open text-primary pe-2"></i> this last question</h6>
+                <p>
+                    very nice question indeed :D ! Lorem Ipsum is simply dummy text of the printing 
+                    and typesetting industry. Lorem Ipsum has been the industry's standard dummy text 
+                    ever since the 1500s, when an unknown printer took a galley of type and scrambled 
+                    it to make a type specimen book.
+                </p>
+                </div>
+            </div>
+            </section>
+            <!--Section: FAQ-->
         </div>
-
-        <div class="col-md-6 col-lg-4 mb-4">
-        <h6 class="mb-3 text-primary"><i class="fas fa-pen-alt text-primary pe-2"></i> question here</h6>
-        <p>
-            <strong><u>good question</u></strong> this is a very good question Lorem Ipsum is simply dummy text of the printing 
-            and typesetting industry. Lorem Ipsum has been the industry's standard dummy text 
-            ever since the 1500s, when an unknown printer took a galley of type and scrambled 
-            it to make a type specimen book.
-        </p>
-        </div>
-
-        <div class="col-md-6 col-lg-4 mb-4">
-        <h6 class="mb-3 text-primary"><i class="fas fa-user text-primary pe-2"></i> A simple
-            question?
-        </h6>
-        <p>
-            this is also a very good question. Lorem Ipsum is simply dummy text of the printing 
-            and typesetting industry. Lorem Ipsum has been the industry's standard dummy text 
-            ever since the 1500s, when an unknown printer took a galley of type and scrambled 
-            it to make a type specimen book.
-        </p>
-        </div>
-
-        <div class="col-md-6 col-lg-4 mb-4">
-        <h6 class="mb-3 text-primary"><i class="fas fa-rocket text-primary pe-2"></i> Something is something
-        </h6>
-        <p>
-            i don't know what to write here. Lorem Ipsum is simply dummy text of the printing 
-            and typesetting industry. Lorem Ipsum has been the industry's standard dummy text 
-            ever since the 1500s, when an unknown printer took a galley of type and scrambled 
-            it to make a type specimen book.
-        </p>
-        </div>
-
-        <div class="col-md-6 col-lg-4 mb-4">
-        <h6 class="mb-3 text-primary"><i class="fas fa-home text-primary pe-2"></i> A simple
-            question?
-        </h6>
-        <p><strong><u>Unfortunately,</u>.</strong> we ran out of questions to fill this example FAQ page . 
-        Lorem Ipsum is simply dummy text of the printing 
-            and typesetting industry. Lorem Ipsum has been the industry's standard dummy text 
-            ever since the 1500s, when an unknown printer took a galley of type and scrambled 
-            it to make a type specimen book.
-        </p>
-        </div>
-
-        <div class="col-md-6 col-lg-4 mb-4">
-        <h6 class="mb-3 text-primary"><i class="fas fa-book-open text-primary pe-2"></i> this last question</h6>
-        <p>
-            very nice question indeed :D ! Lorem Ipsum is simply dummy text of the printing 
-            and typesetting industry. Lorem Ipsum has been the industry's standard dummy text 
-            ever since the 1500s, when an unknown printer took a galley of type and scrambled 
-            it to make a type specimen book.
-        </p>
-        </div>
-    </div>
-    </section>
-    <!--Section: FAQ-->
-</div>
-</body>
+    </body>
 </html>
--- ZAWSS/app/views/Main/index.php
+++ ZAWSS/app/views/Main/index.php
@@ -1,12 +1,16 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
     <head>
-        <title>Some title</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+        <title>Home Page</title>
+        <meta>
+        <!-- CSS only -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+        <!-- JavaScript Bundle with Popper -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     </head>
 
     <body>
-        <?php include 'app\views\includes\header.php'; ?>
+        <?php include 'app\views\includes\userHeader.php'; ?>
         
 
     </body>
