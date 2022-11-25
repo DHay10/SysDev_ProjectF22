e258ab83d6e60f99f57a16ed5c1bfed8205d0a73
DHay10 <son-ta@hotmail.fr>
2022-11-24 20:59:23 -0500

    Updated User NavBar to Bootstrap 5.2

--- ZAWSS/app/views/Includes/userHeader.php
+++ ZAWSS/app/views/Includes/userHeader.php
@@ -10,33 +10,43 @@
         <!-- Nav Items -->
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+
                 <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="/Main/index">Home</a>
+                    <a class="nav-link active" aria-current="page" href="/Main/index">Home</a>
                 </li>
+
                 <li class="nav-item">
-                <a class="nav-link" href="/User/booking">Book</a>
+                    <a class="nav-link" href="/User/booking">Book</a>
                 </li>
+
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         More
                     </a>
+
                     <ul class="dropdown-menu">
                         <li><a class="dropdown-item" href="/Main/aboutUs">About Us</a></li>
                         <li><a class="dropdown-item" href="/Main/faq">FAQ</a></li>
                         <li><a class="dropdown-item" href="/Main/contactUs">Contact Us</a></li>
                     </ul>
                 </li>
+
                 <?php if(isset($_SESSION['user_id'])) { ?>
+                    
                 <li class="nav-item">
                     <a class="nav-link" href="/User/profile">Profile</a>
                 </li>
+
                 <li class="nav-item">
                     <a class="nav-link" href="/User/logout">Log Out</a>
                 </li>
+
                 <?php } else { ?>
+
                 <li class="nav-item">
                     <a class="nav-link" href="/User/login">Sign In</a>
                 </li>
+
                 <?php } ?>
             </ul>
         </div>
