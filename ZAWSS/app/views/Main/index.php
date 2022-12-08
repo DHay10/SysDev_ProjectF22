3a9eafa761ee80a81eb06ee112edb2b6bafb31e0
Md Saqliyan Islam <82405606+SaqSaq815@users.noreply.github.com>
2022-12-07 22:12:24 -0500

    user index modified

--- ZAWSS/app/views/Main/index.php
+++ ZAWSS/app/views/Main/index.php
@@ -14,48 +14,68 @@
     </head>
     <body>
         <?php include 'app\views\includes\userHeader.php'; ?>
-        <div id="slider" name="mx-4">
+        <!-- <div id="slider" name="mx-4"></div> -->
+      
+        <div id="carouselExampleDark" class="carousel carousel-dark slide px-3 py-3" data-bs-ride="carousel">
+          <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="3" aria-label="Slide 4"></button>
+            <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="4" aria-label="Slide 5"></button>
+          </div>
+          <div class="carousel-inner">
+            <div class="carousel-item active" data-bs-interval="8000">
+              <img style='width:100%; aspect-ratio:3/1.25;' src="../images/MtlDowntown.png" class="d-block " alt="../images/MtlDowntown.png">
+              <div class="carousel-caption d-none d-md-block">
+                <!-- <h5>First slide label</h5>
+                <p>Some representative placeholder content for the first slide.</p> -->
+              </div>
+            </div>
 
+            <div class="carousel-item" data-bs-interval="7000">
+              <img style='width:100%; aspect-ratio:3/1.25;' src="../images/Paris.png" class="d-block " alt="../images/Paris.png">
+              <div class="carousel-caption d-none d-md-block">
+                <!-- <h5>Second slide label</h5>
+                <p>Some representative placeholder content for the second slide.</p> -->
+              </div>
+            </div>
+
+            <div class="carousel-item" data-bs-interval="7000">
+              <img style='width:100%; aspect-ratio:5/2;' src="../images/Rome.png" class="d-block w-100" alt="../images/Rome.png">
+              <div class="carousel-caption d-none d-md-block">
+                <!-- <h5>Second slide label</h5>
+                <p>Some representative placeholder content for the second slide.</p> -->
+              </div>
+            </div>
+
+            <div class="carousel-item" data-bs-interval="7000">
+              <img style='width:100%; aspect-ratio:3/1.25;' src="../images/vietnam.png" class="d-block w-100" alt="../images/vietnam.png">
+              <div class="carousel-caption d-none d-md-block">
+                <!-- <h5>Second slide label</h5>
+                <p>Some representative placeholder content for the second slide.</p> -->
+              </div>
+            </div>
+
+            <div class="carousel-item">
+              <img style='width:100%; aspect-ratio:3.5/1.5;' src="../images/morocco.png" class="d-block w-100" alt="../images/morocco.png">
+              <div class="carousel-caption d-none d-md-block">
+                <!-- <h5>Third slide label</h5>
+                <p>Some representative placeholder content for the third slide.</p> -->
+              </div>
+            </div>
+          </div>
+
+          <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
         </div>
 
         <?php include 'app\views\includes\userFooter.php'; ?>
   </body>
-<style type="text/css">
-  ::-webkit-scrollbar{
-    display: none;
-  }
-
-  #slider{
-    background-image: url("../images/MtlDowntown.png");
-    background-size: cover;
-    background-position: center;
-    height: 80vh;    
-    background-repeat: no-repeat;
-    animation-name: change;
-    animation-duration: 10s;
-    animation-iteration-count: infinite;
-    animation-direction: normal;
-    animation-delay: 2s;
-  }
-
-  @keyframes change{
-    0%{
-      background-image: url("../images/MtlDowntown.png");
-    }
-    25%{
-      background-image: url("../images/Paris.png");
-    }
-    50%{
-      background-image: url("../images/morocco.png");
-    }
-    75%{
-      background-image: url("../images/Rome.png");
-    }
-    100%{
-      background-image: url("../images/vietnam.png");
-    }
-  }
-
-
-</style>
 </html>
